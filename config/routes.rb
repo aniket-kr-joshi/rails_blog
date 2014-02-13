143f1cffa8308c423f1318366f5c4c8ac9056762
Aniket Kumar <aniket@aniket.(none)>
2014-02-13 17:30:44 +0530

    Finishing layout and routing

--- config/routes.rb
+++ config/routes.rb
@@ -41,4 +41,5 @@ ActionController::Routing::Routes.draw do |map|
   map.root :controller => 'pages', :action => 'home'
   map.about '/about', :controller=> 'pages', :action=>'about'
   map.contact '/contact', :controller=> 'pages', :action=>'contact'
+  map.signup '/signup', :controller=> 'users', :action=>'new'
 end
